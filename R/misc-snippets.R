069075a6c60de78bfd432e2f998f50655d9ed2ee
Chris Chapman <cnchapman@gmail.com>
2023-02-28 13:18:30 -0800

    more tinkering, removing junk from example site

--- R/misc-snippets.R
+++ R/misc-snippets.R
@@ -1,3 +1,14 @@
+
+blogdown::new_post(title="TITLE",
+                   author="Chris Chapman",
+                   ext = ".Rmd")
+
+
+
+#############
+file.edit(".Rprofile") # edit .Rprofile in HOME
+
+#############
 new_site(theme = "hugo-apero/hugo-apero",
 format = "yaml",
 sample = FALSE,
---
+++ content/post/2023-02-28-title/index.md
@@ -0,0 +1,18 @@
+---
+title: TITLE
+author: Chris Chapman
+date: '2023-02-28'
+slug: []
+categories: []
+tags: []
+---
+test post
+
+
+```r
+set.seed(98250)
+x <- rnorm(1000)
+plot(sort(x))
+```
+
+<img src="{{< blogdown/postref >}}index_files/figure-html/unnamed-chunk-1-1.png" width="672" />
